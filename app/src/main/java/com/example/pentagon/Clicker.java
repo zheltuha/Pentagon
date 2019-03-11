ac6444d7555016b596379e2f8ded5bca6db051e2
zheltuha <voxel.art@yandex.com>
2019-03-12 00:50:25 +0300

    Clicker

--- app/src/main/java/com/example/pentagon/Clicker.java
+++ app/src/main/java/com/example/pentagon/Clicker.java
@@ -4,8 +4,13 @@ import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.zip.CheckedOutputStream;
 
 public class Clicker extends AppCompatActivity {
 
@@ -21,26 +26,26 @@ public class Clicker extends AppCompatActivity {
         imageClick();
     }
 
-    private int sumSuper, speed = 12;
+    private int Clicks, speed = 59;
 
     public void imageClick (){
         tapCkick.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        sumSuper++;
-                        TextView numClick = (TextView)findViewById(R.id.textClickMetr);
-                        TextView speedClick = (TextView)findViewById(R.id.textSpeed);
+                        Clicks++;
+                        TextView sumClick = (TextView)findViewById(R.id.textClickMetr);
+                        TextView speedR = (TextView)findViewById(R.id.textSpeed);
 
-                        numClick.setText("Clicks: "+Integer.toString(sumSuper));
+                        sumClick.setText("Clicks: "+Integer.toString(Clicks));
 
-                        if(sumSuper == 50 || sumSuper == 200){
-                            speed = speed + 110;
-                            speedClick.setText("Speed: "+Integer.toString(speed)+ " 000 km/h");
+                        if(Clicks == 50 || Clicks == 150 || Clicks ==250 || Clicks == 350|| Clicks == 450 || Clicks ==  550){
+                            speed = speed + 93;
+                            speedR.setText("Speed: "+Integer.toString(speed)+ " 000 km/h");
                         }
-
                     }
                 }
         );
     }
+
 }
--- app/src/main/java/com/example/pentagon/Settings.java
+++ app/src/main/java/com/example/pentagon/Settings.java
@@ -1,18 +1,42 @@
 package com.example.pentagon;
 
+import android.gesture.Gesture;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.TextView;
 
-public class Settings extends AppCompatActivity {
+public class Settings extends AppCompatActivity implements
+        GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener{
+
+    private TextView tap;
+    private GestureDetectorCompat detecTap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        assert getSupportActionBar() != null;   //null check
+        assert getSupportActionBar() != null;                    //null check
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);   //show back button
+
+        tap = (TextView)findViewById(R.id.textSettings);
+        detecTap = new GestureDetectorCompat(this, this);
+        detecTap.setOnDoubleTapListener(this);
+
     }
+
+    @Override
+    public boolean onTouchEvent (MotionEvent event){
+        detecTap.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+
     @Override
     public boolean onSupportNavigateUp(){
         finish();
@@ -20,5 +44,51 @@ public class Settings extends AppCompatActivity {
     }
 
 
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        tap.setText("onSingleTapConfirmed" + e.toString());
+        return false;
+    }
 
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        tap.setText("onDoubleTap" + e.toString());
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        tap.setText("onDoubleTapEvent" + e.toString());
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
+    }
 }
